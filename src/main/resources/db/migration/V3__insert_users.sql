ab3d298e50d2f68685a957e06600756c096eddfc
Mark Chepyzhnyi <62542962+mark-chep@users.noreply.github.com>
2026-03-12 21:31:45 +0200

    Update V3__insert_users.sql

--- src/main/resources/db/migration/V3__insert_users.sql
+++ src/main/resources/db/migration/V3__insert_users.sql
@@ -1,4 +1,3 @@
+DELETE FROM users;
 INSERT INTO users (username, password, role, enabled)
-VALUES ('user',
-        '$2a$10$ByIUiNa.nNoer8u6E1Z29u.yT6v7mK9A7Ld4B3jYd.D1Gj0vU9GqO',
-        'user', 1);
+VALUES ('user', '{noop}password', 'user', 1);
