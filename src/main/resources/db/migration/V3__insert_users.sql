a58cb90874e1022020b27d4d84e5d6b9cfa35449
Mark Chepyzhnyi <62542962+mark-chep@users.noreply.github.com>
2026-03-12 21:52:48 +0200

    Update user insert statement with new values

--- src/main/resources/db/migration/V3__insert_users.sql
+++ src/main/resources/db/migration/V3__insert_users.sql
@@ -1,5 +1,5 @@
 DELETE FROM users;
 INSERT INTO users (username, password, role, enabled)
 VALUES ('user', 
-        '$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPZ08zax5AgWu8WtvBW', 
-        'user', 1);
+        '$2a$10$ByIUiNa.nNoer8u6E1Z29u.yT6v7mK9A7Ld4B3jYd.D1Gj0vU9GqO', 
+        'ROLE_USER', 1);
